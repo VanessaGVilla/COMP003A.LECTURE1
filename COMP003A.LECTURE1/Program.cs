96427fe8acfe43187b18e2f0e56e254b619124b5
vanessagarcia <vanessagarcia@LEM231-06>
2023-01-18 18:56:16 -0800

    Add project files.

---
+++ COMP003A.LECTURE1/Program.cs
@@ -0,0 +1,20 @@
+﻿// this is a single-line comment
+/* this is a multi-line comment
+ * Author: Vanessa Garcia
+ * Course: COMP-003A
+ * Purpose: Lecture actiivity for this week
+ */
+
+
+namespace COMP003A.LECTURE1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); // add comments here too
+            Console.WriteLine("This is a string");
+            Console.WriteLine("Vanessa Garcia");
+        }
+    }
+}
